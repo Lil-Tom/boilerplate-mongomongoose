4afffad77eb930a82a8f5bb38f478fa0b9b046b0
Lil-Tom <tsbrown98@gmail.com>
2024-03-06 19:55:21 +1100

    Added ability to search by ID.

--- myApp.js
+++ myApp.js
@@ -55,7 +55,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, (function(err, personFound) {
+    if (err) return console.error(err);
+    done(null, personFound);
+  }))
 };
 
 const findEditThenSave = (personId, done) => {
